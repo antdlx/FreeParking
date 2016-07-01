6d3502f3f408526fae31557c7472d7b82b75dc0f
guduxiaolang <guduxiaolang0225@163.com>
2016-07-01 09:01:50 +0800

    test

---
+++ User/getseller.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2016/7/1
+ * Time: 8:44
+ */
+require_once("common.php");
+
+if($_SERVER["REQUEST_METHOD"]=="POST"){
+    $seller_id = test_input($_POST["seller_id"]);
+    $pdo = connectDB();
+    $stmt = $pdo->prepare("select * from seller where seller_id = ?");
+    $stmt->bindParam(1,$seller_id );
+    $stmt->execute();
+    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+    $obj;
+    $obj["seller_name"] = $row["seller_name"];
+    $obj["seller_img"] = $row["seller_img"];
+    $obj["seller_contact"] = $row["seller_contact"];
+    $obj["seller_address"] = $row["seller_address"];
+    echo json_encode($obj);
+    return;
+}
