c37400ec764ee78c1ad2cc715c75acba3e5e5bff
Pantsu-It <479263213@qq.com>
2016-07-05 15:37:57 +0800

    commit seller

--- Seller/seller_getactlistandcount.php
+++ Seller/seller_getactlistandcount.php
@@ -7,32 +7,37 @@
  */
 require_once("common.php");
 
-if($_SERVER["REQUEST_METHOD"]=="POST"){
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $seller_id = test_input($_POST["seller_id"]);
     $num = test_input($_POST["num"]);
     $pdo = connectDB();
-    $num+=0;
+    $stmt = $pdo->prepare("select seller_address from seller where seller_id = ?");
+    $stmt->bindParam(1, $seller_id);
+    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+    $address = $row["seller_address"];
+    $num += 0;
     $stmt = $pdo->prepare("select * from activity where seller_id = ? order by activity_id limit $num,10");
-    $stmt -> bindParam(1,$seller_id );
+    $stmt->bindParam(1, $seller_id);
     $stmt->execute();
     $rowcnt = $stmt->rowCount();
     $array;
     $i = 0;
-    while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
         $obj["activity_id"] = $row["activity_id"];
         $obj["activity_name"] = $row["activity_name"];
         $obj["activity_starttime"] = $row["activity_starttime"];
         $obj["activity_endtime"] = $row["activity_endtime"];
         $obj["activity_img"] = $row["activity_img"];
         $stmt2 = $pdo->prepare("select * from ticket where activity_id = ?");
-        $stmt2 ->bindParam(1,$row["activity_id"] );
-        $stmt2 ->execute();
+        $stmt2->bindParam(1, $row["activity_id"]);
+        $stmt2->execute();
         $ticketcnt = $stmt2->rowCount();
         $obj["Tcount"] = $ticketcnt;
         $array[$i] = $obj;
         $i++;
     }
     $res["count"] = $rowcnt;
+    $res["seller_address"] = $address;
     $res["array"] = $array;
     echo json_encode($res);
     return;
