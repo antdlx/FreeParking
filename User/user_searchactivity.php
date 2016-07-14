e5d32ed14d3db113077473ceea71c676d6f9e94b
chosenone <15866626260@163.com>
2016-07-14 01:06:14 +0000

    修改用户部分获取商家信息接口： 添加按距离排序

--- User/user_searchactivity.php
+++ User/user_searchactivity.php
@@ -1,11 +1,12 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: chen
- * Date: 16/6/30
- * Time: 下午4:19
+ *
  */
 
+/*
+ *  select SQRT(POW((location_j - 117.1234) * 10,2) + POW((location_w - 36.1234) * 10,2)) as distance from seller;
+ *  select SQRT(POW((location_j - 117.1234) * 10,2) + POW((location_w - 36.1234) * 10,2)) as distance from seller order by distance asc;
+ */
 
 $pdo = new PDO('mysql:host=127.0.0.1;dbname=db_parking;charset=utf8', 'congjiujiu', '123456');
 
@@ -16,25 +17,19 @@ function test_input($data){
     return $data;
 }
 
-$search_word = $_POST['search_word'];
-$number_limit = $_POST['number_limit'];
+$search_word = test_input($_POST['search_word']);
+$number_limit = test_input($_POST['number_limit']);
+$location_j = test_input($_POST['location_j']);
+$location_w = test_input($_POST['location_w']);
 
+$RADIUS = 8;//单位km 显示的商家距离范围
 
-
-$sql = "select count(*) as number from seller where seller_name like '%".$search_word."%' order by seller_id";
+$sql = "select seller_id,seller_name,seller_address,seller_contact,seller_img,SQRT(POW((location_j - $location_j) * 111,2) + POW((location_w - $location_w) * 111,2)) as distance
+from seller where seller_name like '%$search_word%' order by distance limit $number_limit,10";
 
 $stmt = $pdo->prepare($sql);
-
-$stmt->execute();
-
-$row = $stmt->fetch(PDO::FETCH_ASSOC);
-$count = $row['number'];
-
-$stmt = $pdo->prepare("select * from seller where seller_name like '%$search_word%' order by seller_id limit $number_limit,10");
-
 $stmt->execute();
 $count = $stmt->rowCount();
-
 
 
 $i = 0;
@@ -42,18 +37,17 @@ $as;
 $b;
 
 if($count > 0 ){
-    if($count > 10)
-        $b = 10;
-    else
-        $b = $count;
     while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
-        $a['seller_id'] = urlencode($row['seller_id']);
-        $a['seller_name'] = urlencode($row['seller_name']);
-        $a['seller_address'] = urlencode($row['seller_address']);
-        $a['seller_contact'] = urlencode($row['seller_contact']);
-        $a['seller_img'] = urlencode($row['seller_img']);
-        $as[$i] = $a;
-        $i++;
+        if($row['distance'] < $RADIUS){
+            $a['seller_id'] = urlencode($row['seller_id']);
+            $a['seller_name'] = urlencode($row['seller_name']);
+            $a['seller_address'] = urlencode($row['seller_address']);
+            $a['seller_contact'] = urlencode($row['seller_contact']);
+            $a['seller_img'] = urlencode($row['seller_img']);
+            $a['distance'] = urlencode($row['distance']);
+            $as[$i] = $a;
+            $i++;
+        }
     }
     echo urldecode(json_encode($as));
 }
